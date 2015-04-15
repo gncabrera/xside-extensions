d4df288db6b090456e8ba89b81ae3f99c606e782
gcabrera@xsidesolutions.com <gcabrera@xsidesolutions.com>
2015-04-15 18:06:21 -0300

    Added Description attribute for enums. Version 1.2.4

---
+++ System.Extensions.Test/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Extensions;
+
+namespace System.Extensions.Test.Extensions
+{
+    [TestFixture]
+    class EnumExtensionsTests
+    {
+        [Test]
+        public void ICanGetADescriptionFromAnEnum()
+        {
+            Assert.AreEqual("MyPropertyWithDescription", Foo.MyPropertyWithDescription.GetDescription());
+            Assert.AreEqual("Specific", Foo.MyPropertyWithSpecificDescription.GetDescription());
+            Assert.AreEqual("MyPropertyWithoutDescription", Foo.MyPropertyWithoutDescription.GetDescription());
+            Assert.AreEqual("MyPropertyWithEmptyDescription", Foo.MyPropertyWithEmptyDescription.GetDescription());
+        }
+
+        enum Foo {
+            [System.ComponentModel.Description]
+            MyPropertyWithDescription,
+
+            [System.ComponentModel.Description("Specific")]
+            MyPropertyWithSpecificDescription, 
+
+            MyPropertyWithoutDescription,
+
+            [System.ComponentModel.Description("")]
+            MyPropertyWithEmptyDescription
+        }
+    }
+}
---
+++ System.Extensions/Extensions/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<TEnum>(this TEnum EnumValue) where TEnum : struct
+        {
+            FieldInfo fi = EnumValue.GetType().GetField(EnumValue.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0 && !string.IsNullOrWhiteSpace(attributes[0].Description))
+                return attributes[0].Description;
+            else 
+                return EnumValue.ToString();
+        }
+    }
+}
